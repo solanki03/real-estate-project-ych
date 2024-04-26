ffc5b6b800494076bb6a2588b5d9267d8b785183
Solanki Singha <solankisingha23@gmail.com>
2024-04-26 09:51:19 +0530

    26/04/2024, 1st commit.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,17 @@ export default {
   theme: {
     extend: {
       fontSize:{
-        sm:"10.75rem",
+        sm:"0.75rem",
         base:"0.875rem",
         md:"1rem",
         lg:"1.25rem",
         xl:"1.5rem",
         "2xl":"1.75rem",
         "3xl":"2.25rem"
+      },
+
+      backgroundImage: {
+        'team-bg-image': "url('/public/teams/team.png')",
       }
     },
   },
